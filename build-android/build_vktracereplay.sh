d1f4c3f215025a5146eb8fccbb9cb70127f5fc74
Cody Northrop <cnorthrop@google.com>
2017-01-11 10:44:53 -0700

    android: Return early on Linux build script errors

--- build-android/build_vktracereplay.sh
+++ build-android/build_vktracereplay.sh
@@ -11,24 +11,24 @@ fi
 #
 # build layers
 #
-./update_external_sources_android.sh
-./android-generate.sh
-ndk-build -j $cores
+./update_external_sources_android.sh || exit 1
+./android-generate.sh || exit 1
+ndk-build -j $cores || exit 1
 
 #
 # create vkreplay APK
 #
-android update project -s -p . -t "android-23"
-ant -buildfile vkreplay debug
+android update project -s -p . -t "android-23" || exit 1
+ant -buildfile vkreplay debug || exit 1
 
 #
 # build cube-with-layers
 #
 (
-pushd ../demos/android
-android update project -s -p . -t "android-23"
-ndk-build -j $cores
-ant -buildfile cube-with-layers debug
+pushd ../demos/android || exit 1
+android update project -s -p . -t "android-23" || exit 1
+ndk-build -j $cores || exit 1
+ant -buildfile cube-with-layers debug || exit 1
 popd
 )
 
@@ -37,11 +37,11 @@ popd
 #
 (
 pushd ..
-./update_external_sources.sh -g -s
-mkdir -p build
+./update_external_sources.sh -g -s || exit 1
+mkdir -p build || exit 1
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_LOADER=Off -DBUILD_TESTS=Off -DBUILD_LAYERS=Off -DBUILD_VKTRACEVIEWER=Off -DBUILD_LAYERSVT=Off -DBUILD_DEMOS=Off -DBUILD_VKJSON=Off -DBUILD_VIA=Off -DBUILD_VKTRACE_LAYER=Off -DBUILD_VKTRACE_REPLAY=Off -DBUILD_VKTRACE=On ..
-make -j $cores vktrace
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_LOADER=Off -DBUILD_TESTS=Off -DBUILD_LAYERS=Off -DBUILD_VKTRACEVIEWER=Off -DBUILD_LAYERSVT=Off -DBUILD_DEMOS=Off -DBUILD_VKJSON=Off -DBUILD_VIA=Off -DBUILD_VKTRACE_LAYER=Off -DBUILD_VKTRACE_REPLAY=Off -DBUILD_VKTRACE=On .. || exit 1
+make -j $cores vktrace || exit 1
 popd
 )
 
@@ -50,10 +50,10 @@ popd
 #
 (
 pushd ..
-mkdir -p build32
+mkdir -p build32 || exit 1
 cd build32
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_LOADER=Off -DBUILD_TESTS=Off -DBUILD_LAYERS=Off -DBUILD_VKTRACEVIEWER=Off -DBUILD_LAYERSVT=Off -DBUILD_DEMOS=Off -DBUILD_VKJSON=Off -DBUILD_VIA=Off -DBUILD_VKTRACE_LAYER=Off -DBUILD_VKTRACE_REPLAY=Off -DBUILD_X64=Off -DBUILD_VKTRACE=On ..
-make -j $cores vktrace
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_LOADER=Off -DBUILD_TESTS=Off -DBUILD_LAYERS=Off -DBUILD_VKTRACEVIEWER=Off -DBUILD_LAYERSVT=Off -DBUILD_DEMOS=Off -DBUILD_VKJSON=Off -DBUILD_VIA=Off -DBUILD_VKTRACE_LAYER=Off -DBUILD_VKTRACE_REPLAY=Off -DBUILD_X64=Off -DBUILD_VKTRACE=On .. || exit 1
+make -j $cores vktrace || exit 1
 popd
 )
 

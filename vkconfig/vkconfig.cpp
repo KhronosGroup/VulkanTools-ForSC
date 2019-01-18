3a2a28ae9174be6eacc0e39b29afe505401eda7f
Lenny Komow <lenny@lunarg.com>
2019-01-18 13:32:02 -0700

    vkconfig: Make html components optional

--- vkconfig/vkconfig.cpp
+++ vkconfig/vkconfig.cpp
@@ -22,7 +22,10 @@
 #include <QApplication>
 #include <QBoxLayout>
 #include <QFile>
+
+#if !defined(NO_HTML)
 #include <QWebEngineView>
+#endif
 
 int main(int argc, char **argv)
 {
@@ -47,11 +50,13 @@ LayerManager::LayerManager()
     QTabWidget *tab_widget = new QTabWidget();
 
     // Run vulkaninfo and display its output on a tab
+#if !defined(NO_HTML)
     QProcess *vulkan_info = new QProcess(this);
     vulkan_info->setProgram("vulkaninfo");
     vulkan_info->setArguments(QStringList() << "--html");
     vulkan_info->setWorkingDirectory(QDir::temp().path());
     tab_widget->addTab(showHtml(vulkan_info, "Vulkan Info", "vulkaninfo.html"), tr("Vulkan Info"));
+#endif
 
     // Build the layer manager
     QWidget *layer_widget = new QWidget();
@@ -83,7 +88,7 @@ LayerManager::LayerManager()
     tab_widget->addTab(layer_widget, tr("Layer Manager"));
 
     // Run via and display its output on a tab
-#if !defined(__APPLE__)
+#if !defined(__APPLE__) && !defined(NO_HTML)
     QProcess *via = new QProcess(this);
     via->setProgram("vkvia");
     via->setArguments(QStringList() << "--output_path" << QDir::temp().path());
@@ -237,6 +242,7 @@ void LayerManager::timerUpdate()
     notification_label->setPalette(palette);
 }
 
+#if !defined(NO_HTML)
 QWidget* LayerManager::showHtml(QProcess *process, const QString &name, const QString &html_file)
 {
     process->start();
@@ -261,3 +267,4 @@ QWidget* LayerManager::showHtml(QProcess *process, const QString &name, const QS
         return error;
     }
 }
+#endif
